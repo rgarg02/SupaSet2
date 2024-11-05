568fa691e3894dd286be499437e8d1c9770eae5f
Rishi <gargrishi02@gmail.com>
2024-11-05 00:43:34 -0500

    Initial Commit

---
+++ SupaSet/SupaSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupaSetApp.swift
+//  SupaSet
+//
+//  Created by Rishi Garg on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SupaSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SupaSetTests/SupaSetTests.swift
@@ -0,0 +1,16 @@
+//
+//  SupaSetTests.swift
+//  SupaSetTests
+//
+//  Created by Rishi Garg on 11/5/24.
+//
+
+import Testing
+
+struct SupaSetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
